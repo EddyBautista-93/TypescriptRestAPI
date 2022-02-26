a3249e78b395b452e4ddd206d0b37c315b79b312
EddyBautista <bautista.j.eddy@gmail.com>
2022-02-25 22:09:31 -0600

    add logic for getting all post and single post

---
+++ source/routes/posts.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import axios, { AxiosResponse } from 'axios';
+
+// interface is like a contract in a app
+// it defines the syntax for classes to follow.
+
+// interface isn't converted to Javascript. It uses interface for typechecking. 
+interface Post {
+    userId: Number;
+    id: Number;
+    title: String;
+    body: String;
+}
+
+// getting multiple post
+const getPosts = async (req: Request, res: Response, nest: NextFunction) =>{
+    let result: AxiosResponse = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    let posts: [Post] = result.data;
+    return res.status(200).json({
+        message: posts
+    });
+};
+
+// getting single post
+const getPost = async(req:Request, res: Response, next: NextFunction) =>{
+    // get the post id
+    let id: string = req.params.id;
+    // get post
+    let result: AxiosResponse = await axios.get("https://jsonplaceholder.typicode.com/posts/${id}");
+    let post: Post = result.data;
+    return res.status(200).json({
+        message: post
+    });
+};
